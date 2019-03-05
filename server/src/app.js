fa999f1746e13c47c3337ba8e1bc03f07f636005
natgooddev <gooddev639@gmail.com>
2019-03-05 21:41:54 +0700

    model and database with sequelize and sqlite3

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,13 @@
 let express = require('express')
 let bodyParser = require('body-parser')
+const {sequelize} = require('./models')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+require('./routes')(app)
 
 app.get('/status', function (req, res ){
   res.send('Hello nodejs server')
@@ -15,32 +18,11 @@ app.get('/hello/:person', function (req,res) {
   res.send('sey hello with ' + req.params.person)
 })
 
-// get user by id
-app.get('/user/:userId', function (req, res) {
-  res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
-})
-
-// get all user
-app.get('/users', function (req, res) {
-  res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
-})
-
-// create user
-app.post('/user/', function (req, res) {
-  res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
-})
-
-// edit user
-app.put('/user/:userId', function (req, res) {
-  res.send('ทำการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
-})
-
-// delete user
-app.delete('/user/:userId', function (req, res) {
-  res.send('ทำการลบผุ้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
-})
 
 let port = 8081
-app.listen(port, function () {
-  console.log('server running on ' + port)
+
+sequelize.sync({force: false}).then(() => {
+  app.listen(port, function () {
+      console.log('Server running on ' + port)
+  })
 })
--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,26 +1,80 @@
+const {User} = require('../models')
+
 module.exports = {
   // get all user
-  index (req, res) {
-    res.send('เรียกข้อมูล ผู้ใช้งานทั้งหมด')
+  async index (req, res) {
+    try {
+        const users = await User.findAll()
+        res.send(users)
+    } catch (err){
+        res.status(500).send({
+            error: 'The users information was incorrect'
+        })
+    }
   },
 
   // create user
-  create (req, res) {
-    res.send('ทำการสร้างผู้ใช้งาน: ' + JSON.stringify(req.body))
+  async create (req, res) {
+    // res.send(JSON.stringify(req.body))
+    try {
+        const user = await User.create(req.body)
+        res.send(user.toJSON())
+    } catch (err) {
+        res.status(500).send({
+            error: 'Create user incorrect'
+        })
+    }
   },
 
   // edit user, suspend, active
-  put (req, res) {
-    res.send('ทำการแก้ไขผุ้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+  async put (req, res) {
+    try {
+      await User.update(req.body, {
+          where: {
+              id: req.params.userId
+          }
+      })
+      res.send(req.body)
+    } catch (err) {
+        req.status(500).send({
+            error: 'Update user incorrect'
+        })
+    }
   },
 
   // delete user
-  remove (req, res) {
-    res.send('ทำการลบผุ้ใช้งาน: ' + req.params.userId + ' : ' + JSON.stringify(req.body))
+  async remove (req, res) {
+    try {
+      const user = await User.findOne({
+          where: {
+              id: req.params.userId
+          }
+      })
+
+      if(!user){
+          return res.status(403).send({
+              error: 'The user information was incorrect'
+          })
+      }
+
+      await user.destroy()
+      res.send(user)
+    } catch (err) {
+        req.status(500).send({
+            error: 'The user information was incorrect'
+        })
+    }
   },
 
   // get user by id
-  show (req, res) {
-    res.send('ดูข้อมูลผู้ใช้งาน: ' + req.params.userId)
+  async show (req, res) {
+    try {
+      const user = await User.findById(req.params.userId)
+      res.send(user)
+    } catch (err) {
+        req.status(500).send({
+            error: 'The user information was incorrect'
+        })
+    }
   }
 }
